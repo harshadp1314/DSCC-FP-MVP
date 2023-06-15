30fb8e8e59f27e4c6373e053a53633f77a016909
Siva Krishna Reddy Undela <vvsivakrishnareddy@gmail.com>
2023-06-15 10:45:41 -0400

    statisticals and Visualizations analysis

--- DSCC_FP_MVP_Storage.py
+++ DSCC_FP_MVP_Storage.py
@@ -1,44 +1,18 @@
-from boto3.s3.transfer import S3Transfer
+from io import StringIO
 import boto3
 import json
 
-def store_data_in_s3(data):
+def store_df_into_s3(dataframe, file_name):
     # Load credentials from configuration file
     with open("DSCC-FP-MVP-Configuration.json") as config_file:
         config = json.load(config_file)
+
     client = boto3.client('s3', aws_access_key_id=config["aws_access_key_id"],aws_secret_access_key=config["aws_secret_access_key"])
     
-    transfer = S3Transfer(client)
-    transfer.upload_file('C:/Users/vvsiv/Repos/DSCC-FP-MVP/apple_stock.csv', 'dsccfpmvpstorage', 'Data'+"/"+'apple_stock.csv')
+    # Convert DataFrame to CSV format
+    csv_buffer = StringIO()
+    dataframe.to_csv(csv_buffer, index=False)
 
+    client.put_object(Body=csv_buffer.getvalue(), Bucket='dsccfpmvpstorage', Key='{}.csv'.format(file_name))
 
-
-
-
-
-
-
-
-
-
-# import boto3
-# import json
-
-# def store_data_in_dynamodb(data):
-#     # Load credentials from configuration file
-#     with open("DSCC-FP-MVP-Configuration.json") as config_file:
-#         config = json.load(config_file)
-    
-#     # Connect to DynamoDB
-#     dynamodb = boto3.resource(
-#         "dynamodb",
-#         aws_access_key_id=config["aws_access_key_id"],
-#         aws_secret_access_key=config["aws_secret_access_key"]
-#         # region_name=config["region_name"]
-#     )
-    
-#     # Get the table reference
-#     table = dynamodb.Table("dscc-fp-mvp-samsung-stock")
-    
-#     # Store the data in DynamoDB
-#     table.put_item(Item=data)
+    print('{} CSV file saved successfully to S3 bucket'.format(file_name))
